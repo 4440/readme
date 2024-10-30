79618f759fdb817b58b0b7d9d467296cdadfcba8
Steven Deobald <steven@deobald.ca>
2024-10-29 23:55:24 -0300

    goofy little property object
    - for HRM tax property search

--- tax-sales/bot.rb
+++ tax-sales/bot.rb
@@ -5,15 +5,43 @@
 require 'net/http'
 require 'nokogiri'
 
-uri = URI('https://www.halifax.ca/home-property/property-taxes/tax-sale')
+class Property
+  attr_accessor :pid, :href
+  def initialize(pid, href)
+    @pid = pid
+    @href = href
+  end
+
+  def Property.base_url
+    "https://www.halifax.ca/home-property/property-taxes/tax-sale"
+  end
+
+  def pid?
+    pid.to_i > 0 && pid.length == 8
+  end
+
+  def not_pid?
+    !pid?
+  end
+
+  def to_s
+    "PID:#{pid} | HREF:#{Property.base_url}#{href}"
+  end
+
+  def kenney?
+    pid == "00453803"
+  end
+end
+
+uri = URI(Property.base_url)
 uri.freeze
-
 html = Net::HTTP.get(uri)
-# puts page
-
 doc = Nokogiri::HTML.parse(html)
 tags = doc.xpath("//a[@data-entity-type]")
 
-tags.each do |t|
-  puts "#{t[:title]} - #{t[:href]}" if t[:title].to_i > 0 && t[:title].length == 8
-end
+ps = tags
+       .map {|t| Property.new(t[:title], t[:href]) }
+       .delete_if {|p| p.not_pid? }
+
+ps.each {|p| puts p }
+puts "Contains 00453803 (Kenney lot)? - #{ps.any?(&:kenney?)}"

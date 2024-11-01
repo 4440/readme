85b720650db86c539536cc32f75969fbfbddcbf7
Steven Deobald <steven@deobald.ca>
2024-11-01 14:45:07 -0300

    slightly saner parsing, produce a csv

--- tax-sales/bot.rb
+++ tax-sales/bot.rb
@@ -8,40 +8,16 @@ require 'fileutils'
 require 'nokogiri'
 require 'pdf/reader'
 
-class Property
-  attr_accessor :assessment_num, :href
-  def initialize(assessment_num, href)
-    @assessment_num = assessment_num
-    @href = href
-  end
-
-  def assessment_num?
-    assessment_num.to_i > 0 && assessment_num.length == 8
-  end
-
-  def not_assessment_num?
-    !assessment_num?
-  end
-
-  def to_s
-    "Assessment Number:#{assessment_num} | HREF:#{SalePage.base_url}#{href}"
-  end
-
-  def kenney?
-    # TODO: this is obviously wrong
-    assessment_num == "00453803"
-  end
-end
-
-class ScheduleA
+class PdfLink
   attr_accessor :title, :href, :text
-  def initialize(title, href, text)
+  def initialize(title, href, text=nil)
     @title = title
     @href = href
     @text = text
   end
 
   def schedule_a?
+    return false unless @text
     @text.upcase.include? "SCHEDULE A"
   end
 
@@ -49,12 +25,26 @@ class ScheduleA
     !schedule_a?
   end
 
+  def assessment_num?
+    title.to_i > 0 && title.length == 8
+  end
+
+  def not_assessment_num?
+    !assessment_num?
+  end
+
   def to_s
-    "title:#{title} | PDF:#{pdf_url}"
+    return "title:#{title} | PDF:#{pdf_url}" if schedule_a?
+    return "Assessment Number:#{title} | HREF:#{assessment_url}" if assessment_num?
+    "UNKNOWN PDF LINK"
   end
 
   def pdf_url
     "#{SalePage.base_url}#{href}"
+  end
+
+  def assessment_url
+    "#{SalePage.sale_url}#{href}"
   end
 end
 
@@ -79,18 +69,40 @@ class SalePage
     @tags = doc.xpath("//a[@data-entity-type]")
   end
 
-  def properties
+  def deeds
     @tags
-      .map {|t| Property.new(t[:title], t[:href]) }
+      .map {|t| PdfLink.new(t[:title], t[:href]) }
       .delete_if {|p| p.not_assessment_num? }
   end
 
   def list_pdf
     @tags
-      .map {|t| ScheduleA.new(t[:title], t[:href], t.text) }
+      .map {|t| PdfLink.new(t[:title], t[:href], t.text) }
       .delete_if {|p| p.not_schedule_a? }
       .first
       .pdf_url
+  end
+end
+
+class Property
+  attr_accessor :assessment_num, :redeemable
+  def initialize(assessment_num, pids, price, redeemable)
+    @assessment_num = assessment_num
+    @pids = pids
+    @price = price
+    @redeemable = redeemable
+  end
+
+  def pids
+    @pids.join(" & ")
+  end
+
+  def price
+    "$#{@price.to_i + 1}"
+  end
+
+  def kenney?
+    pids.include? "00453803"
   end
 end
 
@@ -130,22 +142,20 @@ class ListingsPdf
   end
 
   def parse
-    puts "Parsing pdf..."
+    puts "Parsing pdf into property listings..."
     reader = PDF::Reader.new(File.expand_path(@file))
     @lines = reader.pages.map {|p| p.text.split(/\n/) }.flatten
-    File.open("zig.txt", 'w') do |f|
-      puts "Assessment No, PID, Price, Redeemable"
-      @lines.each_with_index do |line,i|
-        next if line.include? "Redeemable" # skip the header
-        next if line.strip.length == 0 # skip empty
-        next if line.include? "MOBILE HOME ONLY" # no thx
-        assno = line[0..7]
-        pids = line[77..97]
-        price = line[199..211]
-        redeemable = line[223..225]
-        f.puts "#{assno},#{pids},#{price},#{redeemable}"
-      end
-    end
+    properties = @lines.map do |line|
+      next if line.include? "Redeemable" # skip the header
+      next if line.strip.length == 0 # skip empty
+      next if line.include? "MOBILE HOME ONLY" # no thx
+      assno = line[0..7].strip
+      pids = line[77..97].split(",").map(&:strip)
+      price = line[199..211].gsub("$", "").gsub(",", "").strip
+      redeemable = line[223..225].strip
+      Property.new(assno, pids, price, redeemable)
+    end.compact
+    properties
   end
 end
 
@@ -154,21 +164,25 @@ class Listings
   def initialize(properties)
     @properties = properties
   end
+
   def save_tmp!
     FileUtils.rm_f("latest.tmp")
     File.open("latest.tmp", 'w') do |f|
       puts "Writing listings to temporary file..."
-      @properties.each {|p| f.puts p }
-      f.puts "Contains 00453803 (Kenney lot)? - #{@properties.any?(&:kenney?)}"
+      f.puts "PIDs, Price, Assessment No, Redeemable"
+      @properties.each do |p|
+        f.puts "#{p.pids},#{p.price},#{p.assessment_num},#{p.redeemable}"
+      end
+      f.puts "# Contains 00453803 (Kenney lot)? - #{@properties.any?(&:kenney?)}"
     end
   end
 
   def compare_and_swap!
-    old_listings = File.read("listings.txt") rescue ""
+    old_listings = File.read("listings.csv") rescue ""
     if (old_listings != File.read("latest.tmp"))
-      puts "New listings! Replacing 'listings.txt' and creating dated record..."
-      FileUtils.cp("latest.tmp", "#{Time.now.strftime("%Y-%m-%d")}_listings.txt")
-      FileUtils.cp("latest.tmp", "listings.txt")
+      puts "New listings! Replacing 'listings.csv' and creating dated record..."
+      FileUtils.cp("latest.tmp", "#{Time.now.strftime("%Y-%m-%d")}_listings.csv")
+      FileUtils.cp("latest.tmp", "listings.csv")
       # TODO: commit to git repo (GH action)
       # TODO: send an email
     end
@@ -182,15 +196,13 @@ def main
 
   pdf = ListingsPdf.new(page.list_pdf)
   pdf.download!
-  pdf.parse
 
-  listings = Listings.new(page.properties)
+  listings = Listings.new(pdf.parse)
   listings.save_tmp!
   listings.compare_and_swap!
 end
 
 main()
 
-# TODO: add PIDs to Properties
 # TODO: get kenney/special PID from secrets
 # TODO: git / email stuff in Listings
